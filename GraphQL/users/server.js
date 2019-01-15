4de8f626a88aa5771bd9f3d966079d770eedcdbe
Farooq Majed <farooq306@hotmail.com>
2019-01-15 12:41:29 +0500

    Add schema in server.js

--- GraphQL/users/server.js
+++ GraphQL/users/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const expressGraphQL = require('express-graphql'); //compatibility layer between Express and GraphQL
-const schema = require('./schema/schema')
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
+ 
 const app = express();
 
-app.use('/graphql', expressGraphQL({
+app.use('./graphql', expressGraphQL({
   schema,
   graphiql: true
-}));
+}))
 
 app.listen(4000, () => {
   console.log('Listening');
