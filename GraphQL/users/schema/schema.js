9f659ca233ea17c856e85d0ec2cd29c0dda50f95
farooqmajeed <farooq306@hotmail.com>
2022-03-02 12:01:33 +0500

    added add user mutation

--- GraphQL/users/schema/schema.js
+++ GraphQL/users/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema} = graphql;
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema, GraphQLNonNull } = graphql;
 const axios = require('axios');
 // const _ = require('lodash');
 
@@ -15,21 +15,21 @@ const axios = require('axios');
 //Creating Schema
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields:{
-    id:{ type: GraphQLString },
-    name:{ type: GraphQLString },
-    description:{ type: GraphQLString }
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
   }
 })
-const UserType = new GraphQLObjectType ({
-  name : 'User',
-  fields:{
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-    company:{
-      type:CompanyType,
-      resolve( parentValue,args){
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
           .then(resp => resp.data)
       }
@@ -37,16 +37,34 @@ const UserType = new GraphQLObjectType ({
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLNonNull(GraphQLString) },
+        age: { type: GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, {firstName, age}) {
+        return axios.post('http://localhost:3000/users', { firstName, age })
+          .then(res => res.data)
+      }
+    }
+  }
+})
+
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
-  fields:{
-    user:{
+  fields: {
+    user: {
       type: UserType,
-      args:{id: {type: GraphQLString } },
-      resolve(parentValue, args){
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
-        .then(resp => resp.data)
+          .then(resp => resp.data)
         // return _.find(users, {id: args.id});
         //Now get data from over db.json file which is our local sb server running on localhost:3000
         // .then(response =>console.log(response)) {data:{firstname: 'name}} bad news graphql not know nested property data 
@@ -61,5 +79,6 @@ const RootQuery = new GraphQLObjectType({
 
 //helper
 module.exports = new GraphQLSchema({
-  query : RootQuery
+  query: RootQuery,
+  mutation
 });
